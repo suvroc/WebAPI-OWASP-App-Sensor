b5b6ba733284509ff0b810a24ee55d15b22820c5
Adrian <suvroc@o2.pl>
2016-04-11 22:28:30 +0200

    Add testing API

---
+++ Owasp.AppSensor.Core/Detections/Utilities/HttpMethodHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owasp.AppSensor.Core.Detections.Utilities
+{
+    public class HttpMethodHelper
+    {
+        public void CheckIfMethodIsCorrect()
+        {
+
+        }
+    }
+}
---
+++ Tests/Owasp.AppSensor.Demo.Api/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Owasp.AppSensor.Demo.Api.Controllers
+{
+    [RoutePrefix("api/book")]
+    public class BookController : ApiController
+    {
+        public List<string> GetBooks()
+        {
+            return new List<string>()
+            {
+                "Great Gatsby",
+                "Anumal Farm",
+                "The Catcher in the Rye"
+            };
+        }
+    }
+}
