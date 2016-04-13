837afbe0c05c40b8d0845ba40520da39b1e3ba30
Adrian Sroka <suvroc@o2.pl>
2016-04-13 15:56:04 +0200

    Add OWIN test

---
+++ Tests/Owasp.AppSensor.Demo.Api/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Owasp.AppSensor.Demo.Api.Startup))]
+namespace Owasp.AppSensor.Demo.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
---
+++ Tests/Owasp.AppSensor.Demo.Tests/RE1.TestUnexpectedHttpCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Testing;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Owasp.AppSensor.Demo.Api;
+
+namespace Owasp.AppSensor.Demo.Tests
+{
+    [TestFixture]
+    public class TestUnexpectedHttpCommand
+    {
+        [Test]
+        public async Task TestGoodMethod()
+        {
+            try
+            {
+                using (var server = TestServer.Create<Startup>())
+                {
+                    var result = await server.HttpClient.GetAsync("api/book");
+                    string responseContent = await result.Content.ReadAsStringAsync();
+                    var entity = JsonConvert.DeserializeObject<List<string>>(responseContent);
+
+                    Assert.IsTrue(entity.Count == 3);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
